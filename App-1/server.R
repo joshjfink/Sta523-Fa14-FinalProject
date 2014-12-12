985531040e271b77a82eb2e3c471e8531fe4e189
joshua-james-f <joshua.james.f@gmail.com>
2014-12-12 02:42:27 -0500

    Implementing new interface

--- App-1/server.R
+++ App-1/server.R
@@ -1,37 +1,18 @@
-library(shiny)
+shinyServer(function(input,output){
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+  priors <- reactive({
+    if(input$type=="Continuous") c("Normal", "Uniform", "Exponential", "Inverse-Gamma")
+    else if(input$type=="Discrete") c("Bernoulli", "Binomial", "Poisson")
+    })
   
-  # Expression that generates a histogram. The expression is
-  # wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should re-execute automatically
-  #     when inputs change
-  #  2) Its output type is a plot
-  
-#   output$distPlot <- renderPlot({
-#     x    <- faithful[, 2]  # Old Faithful Geyser data
-#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-#     
-#     # draw the histogram with the specified number of bins
-#     hist(x, breaks = bins, col = 'skyblue', border = 'white')
-#   })
-#   
-  output$contents <- renderTable({
-    
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, it will be a data frame with 'name',
-    # 'size', 'type', and 'datapath' columns. The 'datapath'
-    # column will contain the local filenames where the data can
-    # be found.
-    
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-      return(NULL)
-    
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, 
-             quote=input$quote)
+  output$priorNames <- renderUI({
+    selectInput("Priors","Choose a Prior:",choices=priors(),selected=priors()[1],multiple=T)
   })
 })
+
+# output$dldat <- downloadHandler(
+#     filename = function() { paste(input$dat, '.csv', sep='') },
+#     content = function(file) {
+#       write.csv(dat(), file)
+#     }
+#   )
