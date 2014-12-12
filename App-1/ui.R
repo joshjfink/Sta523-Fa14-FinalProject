89d9d5b6ebd5a72c992f2551907a7d740a83992a
Andrew Wong <aw236@duke.edu>
2014-12-11 20:26:55 -0500

    Added readme to summarize variables.
    Removed some priors.

--- App-1/ui.R
+++ App-1/ui.R
@@ -31,7 +31,7 @@ shinyUI(fluidPage(
       conditionalPanel(
         condition = "input.type == 'disc'",
         selectInput("discType", "Discrete:",
-                    list("Bernoulli", "Binomial", "Poisson", "Geometric")),
+                    list("Bernoulli", "Binomial", "Poisson")),
         ## Discrete
         ## Decision node 2 | Decision node 1
         ## Display appropriate parameters for distribution chosen at node 1.
@@ -47,44 +47,63 @@ shinyUI(fluidPage(
         conditionalPanel(
           condition = "input.discType == 'Poisson'",
           sliderInput("pois.lambda", "Lambda", min = 0, max = 100, value = 1)
-        ),
-        conditionalPanel(
-          condition = "input.discType == 'Geometric'",
-          sliderInput("binom.trials", "Number of Trials", min = 0, max = 100, value = 50),
-          sliderInput("binom.succeses", "Success probability of each trial", min = 0.00, max = 1.00, value = 0.50)  
         )
       ),
       conditionalPanel(
         condition = "input.type == 'cont'",
-        selectInput("contType", "Continuous:",
-                    list("Normal", "Uniform", "Pareto", "Exponential", "Inverse-Gamma")),
+        selectInput(inputId = "contType", 
+                    label = "Continuous:",
+                    choices = list("Normal", "Uniform", 
+                                   "Exponential", "Inverse-Gamma")),
         ## Continuous
         ## Decision node 2 | Decision node 1
         ## Display appropriate parameters for distribution chosen at node 1.
         conditionalPanel(
           condition = "input.contType == 'Normal'",
-          sliderInput("norm.mean", "Mean", min = 0, max = 200, value = 100),
-          sliderInput("norm.var", "Variance", min = 0, max = 200, value = 100)
+          sliderInput(inputId = "norm.mean", 
+                      label = "Mean", 
+                      min = 0, 
+                      max = 200, 
+                      value = 100),
+          sliderInput(inputId = "norm.var", 
+                      label = "Variance", 
+                      min = 0, 
+                      max = 200, 
+                      value = 100)
         ),
         conditionalPanel(
           condition = "input.contType == 'Uniform'",
-          sliderInput("norm.a", "a", min = 0, max = 300, value = 0),
-          sliderInput("norm.b", "b", min = 0, max = 300, value = 100)
-        ),
-        conditionalPanel(
-          condition = "input.contType == 'Pareto'",
-          sliderInput("norm.mean", "Mean", min = 0, max = 400, value = 1),
-          sliderInput("norm.var", "Variance", min = 0, max = 400, value = 1)
+          sliderInput(inputId = "norm.a", 
+                      label = "a", 
+                      min = 0, 
+                      max = 300, 
+                      value = 0),
+          sliderInput(inputId = "norm.b", 
+                      label = "b", 
+                      min = 0, 
+                      max = 300, 
+                      value = 100)
         ),
         conditionalPanel(
           condition = "input.contType == 'Exponential'",
-          sliderInput("norm.mean", "Mean", min = 0, max = 500, value = 1),
-          sliderInput("norm.var", "Variance", min = 0, max = 500, value = 1)
+          sliderInput(inputId = "expo.rate", 
+                      label = "Rate, lambda",
+                      min = 0, 
+                      max = 500, 
+                      value = 1)
         ),
         conditionalPanel(
           condition = "input.contType == 'Inverse-Gamma'",
-          sliderInput("norm.mean", "Mean", min = 0, max = 500, value = 1),
-          sliderInput("norm.var", "Variance", min = 0, max = 500, value = 1)
+          sliderInput(inputId = "invgam.shape", 
+                      label = "Shape, alpha", 
+                      min = 0, 
+                      max = 500, 
+                      value = 1),
+          sliderInput(inputId = "invgam.scale", 
+                      label = "Scale, real", 
+                      min = 0, 
+                      max = 500, 
+                      value = 1)
         )
       )
     ),
